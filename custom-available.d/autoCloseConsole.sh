64e90c8925478246e978c048587836ca128c5497
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-07-07 06:40:51 +0900

    autoexit/noautoexitを追加

--- custom-available.d/autoCloseConsole.sh
+++ custom-available.d/autoCloseConsole.sh
@@ -5,6 +5,8 @@ CHECK_BOL=4
 ps ao ppid,cmd | grep -v 'grep' | grep 'ps ao ppid,cmd' | awk '{print $1}' | read ZTTY_PID
 ZTTY_PATH=$TTY
 
+AUTO_EXIT=1
+
 # Check Proc
 function AutoCloseConsoleChecker(){
     NO_RUN_FLAG=0
@@ -13,7 +15,7 @@ function AutoCloseConsoleChecker(){
         sleep 30
         RUNNING_JOBS=$(ps)
         FIX_RUNNING_JOBS=$(echo $RUNNING_JOBS | tail -n +2 | grep -v 'zsh' | grep -v 'ps')
-        if [[ "$FIX_RUNNING_JOBS" == "" ]]
+        if [[ "$FIX_RUNNING_JOBS" == "" ]] && [[ $AUTO_EXIT -eq 1 ]]
         then
             if [[ $NO_RUN_FLAG -gt $CHECK_BOL ]]
             then
@@ -22,7 +24,7 @@ function AutoCloseConsoleChecker(){
                 echo ""
                 if [[ $NO_RUN_FLAG -eq $CHECK_BOL ]]
                 then
-                    echo ">>> DETECT NO RUN - WILL SOON ALIVE EXIT" > $ZTTY_PATH
+                    echo ">>> DETECT NO RUN - WILL SOON ALIVE EXIT (if u want to be not exit, plz run 'noautoexit' )" > $ZTTY_PATH
                 fi
                 let NO_RUN_FLAG++
             fi
@@ -34,5 +36,7 @@ function AutoCloseConsoleChecker(){
 }
 (AutoCloseConsoleChecker &) > /dev/null 2>&1
 
+alias autoexit="AUTO_EXIT=1"
+alias noautoexit="AUTO_EXIT=0"
 alias exit="kill -9 $CPID ; exit 0"
 
