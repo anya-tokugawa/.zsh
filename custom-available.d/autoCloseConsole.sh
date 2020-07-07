6a79108f31d54f4b0fd6ea5109c9762cb8395faf
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-07-07 14:18:25 +0900

    `autoexit`,`noautoexit`の働かないバグを解消 - coreでAUTO_EXITが有効化検知する手法をやめ、プロセスを操作する手法に変更. - `/dev/null`に吐かせずにコンソール出力にそのまま吐かせるほうに変更。 - `autoexit`有効化は親シェル上で動かす必要があるため`alias`で指定.既に有効な場合は旧いプロセスを`kill` - `noautoexit`無効化は子シェルでも可能なため、`function`で構築 - `exit` 有効時に`exit`をしたときエラるので`noautoexit`処理が終了してから`exit`する

--- custom-available.d/autoCloseConsole.sh
+++ custom-available.d/autoCloseConsole.sh
@@ -1,29 +1,36 @@
 # Auto Close Console Feature
 # 30s * 4 -> 120s = 2min
-CHECK_BOL=4
+A3C_STARTUP=1
+# CheckBowlingTime
+BOW_TIME=10
+CHECK_BOW=4
 
-ps ao ppid,cmd | grep -v 'grep' | grep 'ps ao ppid,cmd' | awk '{print $1}' | read ZTTY_PID
+
+
+
+ZTTY_PID=$$
 ZTTY_PATH=$TTY
-
-AUTO_EXIT=1
-
-# Check Proc
+# Check Process
 function AutoCloseConsoleChecker(){
     NO_RUN_FLAG=0
     while :
     do
-        sleep 30
+        # CheckBowling per 30 seconds.
+        sleep $BOW_TIME
+        # get Process
         RUNNING_JOBS=$(ps)
+        # filtered ps and zsh and HEADER
         FIX_RUNNING_JOBS=$(echo $RUNNING_JOBS | tail -n +2 | grep -v 'zsh' | grep -v 'ps')
-        if [[ "$FIX_RUNNING_JOBS" == "" ]] && [[ $AUTO_EXIT -eq 1 ]]
+        if [[ "$FIX_RUNNING_JOBS" == "" ]]
         then
-            if [[ $NO_RUN_FLAG -gt $CHECK_BOL ]]
+            if [[ $NO_RUN_FLAG -gt $CHECK_BOW ]]
             then
                 break
             else
-                echo ""
-                if [[ $NO_RUN_FLAG -eq $CHECK_BOL ]]
+                if [[ $NO_RUN_FLAG -eq $CHECK_BOW ]]
                 then
+                    echo ""
+                    echo "AUTO_EXIT -> $AUTO_EXIT"
                     echo ">>> DETECT NO RUN - WILL SOON ALIVE EXIT (if u want to be not exit, plz run 'noautoexit' )" > $ZTTY_PATH
                 fi
                 let NO_RUN_FLAG++
@@ -34,9 +41,18 @@ function AutoCloseConsoleChecker(){
     done
     kill -9 $ZTTY_PID
 }
-(AutoCloseConsoleChecker &) > /dev/null 2>&1
 
-alias autoexit="AUTO_EXIT=1"
-alias noautoexit="AUTO_EXIT=0"
-alias exit="kill -9 $CPID ; exit 0"
-
+if [ $A3C_STARTUP -eq 1 ]
+then
+    echo -n "A3C STARTUP Enabled - "
+    AutoCloseConsoleChecker &
+    A3C_PID=$!
+fi
+alias autoexit='noautoexit ; wait && echo -en "\e[38;5;082m A3C" ; AutoCloseConsoleChecker & A3C_PID=$!'
+function noautoexit(){
+    if [[ "$A3C_PID" != "" ]] && [[ $(ps $A3C_PID | tail -n +2) != "" ]]
+    then
+            echo -en "\e[38;5;196m A3C" ; kill -9 "$A3C_PID"
+    fi
+}
+alias exit='noautoexit ; wait && exit'
