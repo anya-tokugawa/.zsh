59f105de7ad8a8f20230ad32c29b4966559dd062
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-08-01 21:41:16 +0900

    if exist_command -> aliased!

--- custom-available.d/changeAlternativeTools.sh
+++ custom-available.d/changeAlternativeTools.sh
@@ -1,3 +1,10 @@
-alias ls="exa"
-#alias find="fd"
-#alias grep="rg"
+
+_hasCommand () {
+  type "$1" > /dev/null 2>&1 && return 0
+  return 1
+}
+
+if _hasCommand exa ; then 
+  alias ls="exa"
+  alias la="exa -a" ; fi
+if _hasCommand fd  ; then alias find="fd" ; fi
