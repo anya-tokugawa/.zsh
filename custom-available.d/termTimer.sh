29170d2da490e81ee5d5628094c00c8fa5a57b86
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-08-23 15:24:06 +0900

    add usage

--- custom-available.d/termTimer.sh
+++ custom-available.d/termTimer.sh
@@ -12,10 +12,16 @@ sed -i '/^'"$1"'/d' $HOME/.timer.csv
 }
 
 timer(){
+  if [[ $# == 0 || $1 == '-h' ]];
+  then
+    echo "usage: timer 'subject_str' wait_secound."
+    return
+  fi
   udate=$(date)
   utime=$(date '+%s')
   (timer_core $utime $1 $2 &)>/dev/null 2&>1
   timer_pid=$!
+  # CSV: UNIX_TIME,DATE_STR,SUBJECT,TIME_SECONDS,PID
   echo "$utime,$udate,$2,$1,$timer_pid" >> $HOME/.timer.csv
   echo "RUN - $utime $timer_pid"
 }
