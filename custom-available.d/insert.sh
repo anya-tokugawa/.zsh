458b317ca92d2ff8abd8c3fc76cecb000e52bbb4
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-08-23 15:36:07 +0900

    add custom feature: insert commands

---
+++ custom-available.d/insert.sh
@@ -0,0 +1,20 @@
+function insert(){
+  if [[ $# == 0 || $1 == '-h' ]]
+  then
+    echo "usage: insert 'target_texts' 'relative_file_path'"
+    return
+  fi
+  if [[ -f ./"$2" ]]
+  then
+    echo "$1" >> ./"$2" && echo "SUCCESSED!"
+  else
+    echo -n "Attension: file is not exist. continue? (continue as 'y'): "
+    read check
+    if [[ "$check" == 'y' ]]
+    then
+      echo "$1" >> ./"$2" && echo "SUCCESSED!"
+    else
+      echo "CHANSELED!"
+    fi
+  fi
+}
