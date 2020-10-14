be90f6d5b57357f739d02b0bea7dbf56759738b6
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-10-14 18:53:35 +0900

    aptに依存する箇所を除去し、ないコマンドはWARNINGに変更

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,19 +1,23 @@
 #!/bin/bash -eu
 
+_hasCommand () {
+  type "$1" > /dev/null 2>&1 && return 0
+  return 1
+}
+
 echo -n "Input current workspace name -> "
 read ws_name
+
 echo "export ZSH_WORKSPACE=""'""$ws_name""'" >> config
 
 # Dependency
 
+_hasCommand zsh  || echo "WARNING: zsh  is not found!"
+_hasCommand perl || echo "WARNING: perl is not found!"
+_hasCommand git  || echo "WARNING: git  is not found!"
 
-sudo apt install \
-    zsh \
-    language-pack-ja \
-    perl
-    git
+curl -fsSL git.io/antigen > $HOME/.zsh/custom-available.d/antigen.zsh
 
-curl -L git.io/antigen > $HOME/.zsh/custom-available.d/antigen.zsh
 ln -s $HOME/.zsh/.zshenv $HOME/
 
 mkdir -p $HOME/.zsh/custom-enable.d/
