d1273bb324b72960fb17102c67020d9a2accfde9
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-10-20 13:13:22 +0900

    add tags feature

--- custom-available.d/task.sh
+++ custom-available.d/task.sh
@@ -4,35 +4,52 @@ export _indexFile="${TASK_DIR}/index.psv"
 export _doneFile="${TASK_DIR}/done.psv"
 ##################################################
 mkdir -p "$TASK_DIR/done.d/"
-if [[ ! -e ${TASK_DIR}/.git ]]; then
+
+if [[ ! -e ${TASK_DIR}/.git ]]
+then
   git init $TASK_DIR
 fi
 
 
-function _task_list_verbose(){
+function _task_list_verbose() {
   cnt=0
-  echo "No.|task|add date|detail"
-  echo "-----|------------|---------------------|---------"
+  if [[ "$1" == "verbose" ]]
+  then
+    echo "No.|task|add date|detail|tags|uuid"
+    echo "-----|----------------------|---------------------|---------|--------|----------------------"
+  else
+    echo "No.|task|add date|detail|tags"
+    echo "-----|----------------------|---------------------|---------|--------"
+  fi
   for file in $(cut -d'|' -f1 $_indexFile )
   do
     cnt=$(echo "$cnt + 1" | bc)
     source "${TASK_DIR}/${file}.source"
-    echo "$cnt|$_task_name|$_task_add_date $_task_add_time|$_task_more"
+    if [ "$1" = "verbose" ]
+      then echo " $cnt| $_task_name| $_task_add_date $_task_add_time| $_task_more| $_task_tags| $file"
+      else echo " $cnt| $_task_name| $_task_add_date $_task_add_time| $_task_more| $_task_tags"
+    fi
   done
 }
+
 function _task_add(){
   if [[ $1 == '' ]]; then echo "plz name."; return 1; fi
   _id=$(uuidgen)
   echo "$_id|$1" >> $_indexFile
   _targetFile="${TASK_DIR}/${_id}.source"
-  echo "_task_name='$1'" >> $_targetFile
-  echo "_task_more='$2'" >> $_targetFile
+  fix_name="$(echo $1 | sed -e "s;\';;g" )"
+  fix_more="$(echo $2 | sed -e "s;\';;g")"
+  echo "_task_name=\"$fix_name\"" >> $_targetFile
+  echo "_task_more=\"$fix_more\"" >> $_targetFile
   echo "_task_add_date='$(date +%Y-%m-%d)'" >> $_targetFile
   echo "_task_add_time='$(date +%H:%M:%S)'" >> $_targetFile
+  echo "_task_tags=" >> $_targetFile
   echo "New Task to $_targetFile"
   git -C ${TASK_DIR} add $_targetFile $_indexFile
   git -C ${TASK_DIR} commit -m"ADD: $1 <$_id>"
 }
+
+
 function _task_done(){
   if [[ $1 == '' ]]; then echo "plz id."; return 1; fi
   _id=$(awk "NR==$1" $_indexFile | cut -d'|' -f1)
@@ -43,8 +60,12 @@ function _task_done(){
   echo "done $_name"
   git -C ${TASK_DIR} add $_indexFile $_doneFile
   git -C ${TASK_DIR} commit -m"DONE: $_name <$_id>"
-  if [[ "$_name" == "$TASK" ]]; then TASK="" fi
+  if [[ "$_name" == "$TASK" ]]
+  then
+    TASK=""
+  fi
 }
+
 function _task_delete(){
   if [[ $1 == '' ]]; then echo "plz id."; return 1; fi
   _id=$(awk "NR==$1" $_indexFile | cut -d'|' -f1)
@@ -54,16 +75,49 @@ function _task_delete(){
   echo "deleted $_name <$_id>"
   git -C ${TASK_DIR} add $_indexFile
   git -C ${TASK_DIR} commit -m"DEL: $_name"
-  if [[ "$_name" == "$TASK" ]]; then TASK="" fi
+  if [[ "$_name" == "$TASK" ]]
+  then
+      TASK=""
+  fi
 }
 ##################################################
 function _task_get(){
   if [[ $1 == '' ]]; then echo "plz id."; return 1; fi
   _id=$(awk "NR==$1" $_indexFile | cut -d'|' -f1)
   source "${TASK_DIR}/${_id}.source"
-  echo "Task Name: $_task_name"
+  echo "New Task to $_targetFile"
   echo "Detail:    $_task_more"
   echo "Add Date:  $_task_add_date $_task_add_time"
+  echo "TAGS: $_task_tags"
+}
+
+function _task_tags(){
+  if [[ $1 == '' ]]; then echo "plz id."; return 1; fi
+
+  _id=$(awk "NR==$1" $_indexFile | cut -d'|' -f1)
+  source "${TASK_DIR}/${_id}.source"
+  if [[ $2 == '' ]]
+  then
+    echo "$_task_tags"
+    return 0
+  fi
+
+  for tag in ${@:2}
+  do
+    [ "$_task_tags" != '' ] && _task_tags=" $_task_tags"
+    tag_name=$(echo $tag | cut -c 2- )
+    tag_prefix=$(echo $tag | cut -c 1 )
+    [ "$tag_prefix" = "+" ] && _task_tags="$tag_name$_task_tags"
+    [ "$tag_prefix" = "-" ] && _task_tags="$(echo $_task_tags | sed -e s;\ $tag_name\ ;\ ;g )"
+  done
+
+  _targetFile="${TASK_DIR}/${_id}.source"
+  echo -n '' > $_targetFile
+  echo "_task_name=\"$_task_name\"" >> $_targetFile
+  echo "_task_more=\"$_task_more\"" >> $_targetFile
+  echo "_task_add_date=\"$_task_add_date\"" >> $_targetFile
+  echo "_task_add_time=\"$_task_add_time\"" >> $_targetFile
+  echo "_task_tags=\"$_task_tags\"" >> $_targetFile
 }
 
 function _task_pin(){
@@ -80,13 +134,26 @@ function _task_show(){
   then
     clear
   fi
-  _task_list_verbose  | column -ts'|'
+  if [ "$1" = "raw" ]
+  then
+    _task_list_verbose $@
+  else
+    if [ "$1" = "grep" ]
+    then
+      _task_list_verbose $@ | column -ts'|' | head -n1
+      _task_list_verbose $@ | column -ts'|' | grep ${@:2}
+    else
+      _task_list_verbose $@ | column -ts'|'
+    fi
+  fi
 }
-function _task_tsync(){
-  git -C ${TASK_DIR} pull
+function _task_sync(){
+  echo "sync .task git-repo."
+  git -C "${TASK_DIR}" pull
   test $? -ne 0 && return 1
-  git -C ${TASK_DIR} push
+  git -C "${TASK_DIR}" push
 }
+
 function _task_log(){
   git -C ${TASK_DIR} log
 }
@@ -102,20 +169,23 @@ COMMAND:
  pin [id]
  sync
  log
+ tags [id] [+ADD_TAG] [-REMOVE_TAG]
+ grep [grep_some_opt]
  help
 HELP_TEXT
 }
 
 function t(){
   case "$1" in
-    "add")  _task_add $2 $3 ;;
+    "add")  _task_add ${@:2} ;;
     "done") _task_done $2 ;;
-    "del")  _task_del $2 ;;
+    "del")  _task_delete $2 ;;
     "get")  _task_get $2 ;;
     "pin")  _task_pin $2 ;;
     "sync") _task_sync ;;
     "log")  _task_log  ;;
+    "tags")  _task_tags ${@:2} ;;
     "help") _task_help ;;
-    *)      _task_show ;;
+    *)      _task_show $@;;
   esac
 }
