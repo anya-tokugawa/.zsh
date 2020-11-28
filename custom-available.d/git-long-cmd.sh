68dfdcd83ed73a41729d2bbcd64e3e55b282fd85
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-11-28 16:19:30 +0900

    add git-alias-command

---
+++ custom-available.d/git-long-cmd.sh
@@ -0,0 +1,14 @@
+function git-remove-all-history(){
+git filter-branch --force --index-filter "git rm --cached --ignore-unmatch $@" --prune-empty --tag-name-filter cat -- --all
+}
+function git-oneline(){
+  if [[ $# -ne 2 ]]
+  then
+    echo "usage: git-onelne [COMMIT_MESG] [FILE]"
+    echo "proc: add file and commit with commit-mesg"
+    return 1
+  fi
+  git add $2
+  git commit -m"$1"
+
+}
