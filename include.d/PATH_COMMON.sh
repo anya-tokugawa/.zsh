663c1196f9181bf663558b3356645d24dcdbe63b
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2021-03-15 04:25:18 +0900

    ADD: LD_LIBRARY_PATH_LIST

--- include.d/PATH_COMMON.sh
+++ include.d/PATH_COMMON.sh
@@ -8,9 +8,17 @@ $HOME/.nesc/bin
 EOF
 )
 export PATH="$PATH_LIST:$PATH"
-
+PATH_LIST=$(cat << EOF | xargs | sed -e 's/\ /:/g'
+$HOME/lib
+$HOME/.local/lib
+$HOME/local/lib
+EOF
+)
+export LD_LIBRARY_PATH="$PATH_LIST:${LD_LIBRARY_PATH}"
 # .local/packages
 for i in $(/bin/ls -1 $HOME/.local/packages/)
 do
   export PATH="$HOME/.local/packages/${i}/bin:$PATH"
+  export LD_LIBRARY_PATH="${HOME}/.local/packages/${i}/lib:${LD_LIBRARY_PATH}"
+  export LD_LIBRARY_PATH="${HOME}/.local/packages/${i}/lib64:${LD_LIBRARY_PATH}"
 done
