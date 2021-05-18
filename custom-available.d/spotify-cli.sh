46abfd0d0ca4c48caf24c1b9ba30f61e5958cdfe
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2021-05-18 17:48:12 +0900

    Add Spotify CLI Shortcut with pip3-spotify-cli - liked Discord Commands. - Check Commands '-help' - Need to Install 'spotify' from pip3

---
+++ custom-available.d/spotify-cli.sh
@@ -0,0 +1,18 @@
+if $(spotify > /dev/null 2>&1 ); then
+  function -next     { spotify next   $@ }
+  function -pause    { spotify pause  $@ }
+  function -play     { spotify play   $@ }
+  function -previous { spotify previous  $@ }
+  function -queue    { spotify queue  $@ }
+  function -repeat   { spotify repeat  $@ }
+  function -shuffle  { spotify shuffle  $@ }
+  function -vol      { spotify volume  $@ }
+  function -status   { spotify status  $@ }
+  function -history  { spotify history  $@ }
+  function -search   { spotify search  $@ }
+  function -top      { spotify top  $@ }
+  function -save     { spotify save  $@ }
+  function -help {
+    echo "-next -pause -play -previous -queue -repeat -shuffle -vol -status -history -search -top -save -help"
+  }
+fi
