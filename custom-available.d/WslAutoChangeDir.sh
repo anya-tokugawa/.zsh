311b5356613f21e3ffaf80758f618570287c3b22
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2022-05-20 20:12:23 +0900

    FIX(Extension): Fix Detection Path

--- custom-available.d/WslAutoChangeDir.sh
+++ custom-available.d/WslAutoChangeDir.sh
@@ -1,12 +1,16 @@
 # Descripton: WSL Auto ChangeDirt to HOME
 export ZTTY_FEATURES="WslAutoChangeDir:${ZTTY_FEATURES}"
 # DETECT WINDOWS STARTUP_PATH
-if [[ "$PWD" =~ "/mnt/c/.*/eric/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup" ]]
+if [[ "$PWD" =~ "/mnt/c/.*/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup" ]]
 then
 	cd $HOME >> /dev/null
 fi
 # If Windows Terminal
 if [[ "$PWD" =~ "/mnt/c/.*/eric" ]]
+then
+	cd $HOME >> /dev/null
+fi
+if [[ "$PWD" =~ "/mnt/c/Windows/system32" ]]
 then
 	cd $HOME >> /dev/null
 fi
